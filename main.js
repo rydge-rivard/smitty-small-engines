ef85e6b4c27fba5e1218099af92ad93222860a03
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-11-30 15:58:14 +0100

    Add bike wheelie transition.

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+let bike = document.querySelector(".bike");
+window.addEventListener("scroll", function () {
+  let value = window.scrollY * 0.1;
+  bike.style.transform = `translateX(${
+    -100 + value * 6
+  }px) rotate(-${value}deg)`;
+});
